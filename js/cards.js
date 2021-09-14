5f83e8c9e06a3b07f46835d08446098c8e4babb7
WilliamDeckmann <u773560@rts-365.dk>
2021-09-14 14:24:57 +0200

    Add filte.js & cards.js & update index

---
+++ js/cards.js
@@ -0,0 +1,106 @@
+document.addEventListener("DOMContentLoaded", () => {
+    
+    // Element variable
+    const container = document.querySelector(".Cards-container__container");
+
+    // Fetch data
+    fetch("./json/residence.json")
+        .then((response) => {
+            /* if(!response.ok) {
+                throw new Error("Ups, noget gik galt!");
+            } */
+            return response.json();
+        })
+        .then((residenceData) => {
+
+            residenceData.residences.forEach((residence) => {
+
+                // Create element, add content & append it
+                let cardLink = document.createElement("a");
+                container.appendChild(cardLink);
+                cardLink.href = "./about.html";
+                cardLink.classList.add("Card-link");
+                cardLink.innerHTML =
+                `<!-- Card -->
+                <article class="Card">
+    
+                    <!-- Card - img-container -->
+                    <figure class="Card__img-container">
+    
+                        <!-- Card - img & tag -->
+                        <img class="Card__img" src="./img/residence_01/${residence.images[0]}" alt="Billede af udlejing">
+                        <div class="Card__tag">
+                            <p class="Card__tag-title uppercase font-bold">
+                                Vis alle billeder
+                            </p>
+                        </div>
+                    </figure>
+    
+                    <!-- Card - info -->
+                    <section class="Card__info">
+    
+                        <!-- Card - header -->
+                        <header class="Card__header">
+    
+                            <!-- Card - header-left -->
+                            <section class="Card__header-left">
+    
+                                <!-- Residence-description -->
+                                <p class="Residence-description color-weak">
+                                    ${residence.details.about.description}
+                                </p>
+    
+                                <!--  Residence-title -->
+                                <h2 class="Residence-title Card__residence-title font-thin">
+                                    ${residence.details.about.title} - ${residence.details.about.tag}
+                                </h2>
+
+                                <!--  Residence-line -->
+                                <hr class="Residence-line line">
+    
+                                <!-- Rooms-list -->
+                                <ul class="Facilities-list color-weak">
+    
+                                </ul>
+                            </section>
+    
+                            <!-- Card - header-right -->
+                            <section class="Card__header-right">
+                                <i class="far fa-heart Heart Icon Card__heart"></i>
+                            </section>
+                        </header>
+    
+                        <!-- Residence-rating -->
+                        <footer class="Card__footer">
+    
+                            <!-- Star & Rating -->
+                            <p class="Rating font-bold">
+                                <i class="${residence.details.mention.icon}"></i>
+                                    ${residence.details.mention.rating}
+                            </p>
+    
+                            <!-- Mention -->
+                            <p class="Mention color-weak">
+                                (${residence.details.mention.amount} ${residence.details.mention.title})
+                            </p>
+                        </footer>
+                    </section>
+                </article>`;
+
+
+                
+                // Element variable
+                let facilitiesList = document.querySelector(".Facilities-list");
+
+                // For each Facilities-list item
+                residence.details.facilities.items.forEach((item) => {
+
+                    // Create, append & add content
+                    let facility = document.createElement("li")
+                    facilitiesList.appendChild(facility);
+                    facility.classList.add("Facilities-item");
+                    facility.innerHTML = `${item.amount} ${item.title}`
+                });
+            });
+        })
+});
--- js/filters.js
+++ js/filters.js
@@ -1,49 +1,50 @@
 document.addEventListener("DOMContentLoaded", () => {
     
     // Element variable
-    const filter = document.querySelector(".Cards-container__info");
+    const header = document.querySelector(".Cards-container__header");
+    const filtersContainer = document.querySelector(".Cards-container__filters-container");
 
     // Fetch data
-    fetch("./json/residence.json")
+    fetch("./json/filters.json")
         .then((response) => {
             /* if(!response.ok) {
                 throw new Error("Ups, noget gik galt!");
             } */
             return response.json();
         })
-        .then((residenceData) => {
+        .then((filtersData) => {
 
-            // Console log
-            console.log(residenceData)
-
-            // Inner html
-            filter.innerHTML =
+            // Add header inner html
+            header.innerHTML =
             `<!-- Cards-container - result-count & title -->
             <p class="Cards-container__result-count">
-            Over ${residenceData.filters.about.amount} overnatningssteder
+                Over ${filtersData.about.amount} overnatningssteder
             </p>
             <h2 class="Cards-container__title">
-                ${residenceData.filters.about.title}
-            </h2>
+                ${filtersData.about.title}
+            </h2>`;
 
-            <!-- Cards-container - filters-container -->
-            <ul class="Cards-container__filters-container">
-                <li class="Cards-container__filter">
-                    ${residenceData.filters.filters.items[0].title}
-                </li>
-                <li class="Cards-container__filter Cards-container__filter_active">
-                    ${residenceData.filters.filters.items[1].title}
-                </li>
-                <li class="Cards-container__filter">
-                    ${residenceData.filters.filters.items[2].title}
-                </li>
-                <li class="Cards-container__filter">
-                    ${residenceData.filters.filters.items[3].title}
-                </li>
-                <li class="Cards-container__filter ${residenceData.filters.filters.count.active.class}">
-                    ${residenceData.filters.filters.count.title} · ${residenceData.filters.filters.count.amount}
-                </li>
-            </ul>`;
+            // Add item for for each object in array
+            filtersData.filters.items.forEach((item) => {
+                
+                // Create, append & add content
+                let filter = document.createElement("li");
+                filtersContainer.appendChild(filter);
+                filter.classList.add("Cards-container__filter");
+                filter.innerHTML = item.title;
+                
+                // Check for state
+                if(item.active.type == true) {
+                    filter.classList.add(item.active.class)
+                }
+            });
+
+            // Create, append & add content
+            let counter = document.createElement("li");
+            filtersContainer.appendChild(counter);
+            counter.classList.add("Cards-container__filter");
+            counter.classList.add(filtersData.filters.count.active.class);
+            counter.innerHTML = `${filtersData.filters.count.title} · ${filtersData.filters.count.amount}`
         })
         /* .catch((error) => {
             console.log(error.message);
